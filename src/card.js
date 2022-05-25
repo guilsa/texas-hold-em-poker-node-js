3febbc55c67cb3df53f09d2f200a51b8d85076d1
guilsa <guilsa001@gmail.com>
2022-05-25 00:21:39 -0700

    Add tests for valid suit and ranks

--- src/card.js
+++ src/card.js
@@ -1,7 +1,19 @@
+const SUITS = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
+const RANKS = ['2', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
 class Card {
   constructor(rank, suit) {
+    if (!RANKS.includes(rank)) throw new Error(`Invalid rank. Rank must be one of the following ${RANKS}`)
+
+    if (!SUITS.includes(suit)) throw new Error(`Invalid suit. Suit must be one of the following ${SUITS}`)
     this.rank = rank
-    this.suite = suit
+    this.suit = suit
+    this.SUITS = SUITS
+    this.RANKS = RANKS
+  }
+
+  toString() {
+    return `${this.rank} of ${this.suit}`
   }
 }
 
--- test/card.test.js
+++ test/card.test.js
@@ -15,3 +15,21 @@ it('has string representation with name and suit', () => {
   const card = new Card('5', 'Diamonds')
   assert.equal(card.toString(), '5 of Diamonds')
 })
+
+it('card has four possible suit options', () => {
+  const card = new Card('5', 'Diamonds')
+  assert.deepEqual(card.SUITS, ['Hearts', 'Clubs', 'Spades', 'Diamonds'])
+})
+
+it('card has thirteen possible rank options', () => {
+  const card = new Card('5', 'Diamonds')
+  assert.deepEqual(card.RANKS, ['2', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'])
+})
+
+it('card only allows for valid rank', () => {
+  assert.throws(() => new Card('Two', 'Diamonds'), Error)
+})
+
+it('card only allows for valid suit', () => {
+  assert.throws(() => new Card('2', 'Ducks'), Error)
+})
